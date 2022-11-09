bb9de10019fa5d6b9d464608f91f37b50dbb127d
Alina Lavrik <alina_voro@mail.ru>
2022-11-09 17:16:52 +0300

    Add example 003

---
+++ Example_003/Program.cs
@@ -0,0 +1,14 @@
+﻿// O (n^2)
+// Таблица умножения
+int n = Convert.ToInt32(Console.ReadLine()); // Введем число и укажем его
+for(int i = 1; i <= n; i++)
+{
+    for(int j = 1; j <= n; j++)
+    {
+        Console.Write(i * j);
+        //Console.Write(" ");
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
+
