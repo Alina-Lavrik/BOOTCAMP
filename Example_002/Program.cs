8dfc89313328a86ed7e29a2e3b0e36f309a1b974
Alina Lavrik <alina_voro@mail.ru>
2022-11-09 17:16:30 +0300

    Add example 002

---
+++ Example_002/Program.cs
@@ -0,0 +1,13 @@
+﻿// массив [ 4, 5, 3, 1, 2]
+// O(n) - количество операций, время работы
+// сумма элементов массива
+int n = 5;
+int[] array = new int[n];  
+for (int i = 0 ; i < n; i++)                           
+      array[i] = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("[" + string.Join(", ", array) + "]");  
+
+int summa = 0;
+for (int i = 0; i < n; i++)
+    summa += array[i];
+Console.WriteLine(summa);
